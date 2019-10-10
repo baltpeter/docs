4b3c706a9e3b1c32b15e39226cb6cf0b0d111f41
Benjamin Altpeter <benjamin@oldpeter.de>
2019-10-10 14:18:28 +0200

    Sys/Win10: Extend bloatware removal script
    Also unprovision apps.

--- content/systems/windows/uninstall-bloatware.ps1
+++ content/systems/windows/uninstall-bloatware.ps1
@@ -2,46 +2,77 @@
 param([switch]$Elevated)
 
 function Test-Admin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) 
-    {
-        # tried to elevate, did not work, aborting
-    } 
-    else {
+if ((Test-Admin) -eq $false) {
+    if (-Not $elevated) {
         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+
+    exit
 }
 
-exit
+$ToRemove =
+    "*Store.Engagement*",
+    "*windowscommunications*",
+    "*Advertising*",
+    "*3DBuilder*",
+    "*3DViewer*",
+    "*Print3D*",
+    "*MixedReality*",
+    "*Solitaire*",
+    "*Bing*",
+    "*WindowsFeedbackHub*",
+    "*Microsoft.GetHelp*",
+    "*Microsoft.Getstarted*",
+    "*Microsoft.Wallet*",
+    "*Microsoft.Messaging*",
+    "*Microsoft.MicrosoftOfficeHub*",
+    "*Microsoft.Office*",
+    "*Microsoft.OneConnect*",
+    "*Microsoft.People*",
+    "*OneNote*",
+    "*Skype*",
+    "*Xbox*",
+    "*Zune*",
+    "*Adobe*",
+    "*Duolingo*",
+    "*News*",
+    "*Sway*",
+    "*CandyCrush*",
+    "*Wunderlist*",
+    "*Netflix*",
+    "*Twitter*",
+    "*Facebook*",
+    "*Amazon*",
+    "*Pandora*",
+    "*Flipboard*"
+
+$confirmation = Read-Host "Also uninstall the Windows Store? [y/n]"
+if ($confirmation -eq 'y') {
+    $ToRemove +=
+        "*WindowsStore*",
+        "*DesktopAppInstaller*",
+        "*StorePurchaseApp*"
 }
 
+Write-Host "[Benni_win10-unprovision] " -ForegroundColor Blue -NoNewline
+Write-Host Unprovisioning Windows 10 bloatware...
+foreach ($app in $ToRemove) {
+    Write-Host "[Benni_win10-unprovision] " -ForegroundColor Blue -NoNewline
+    Write-Host Unprovisioning $app...
+    Get-AppxProvisionedPackage -Online | where DisplayName -like $app | Remove-AppxProvisionedPackage -Online
+}
+Write-Host "[Benni_win10-unprovision] " -ForegroundColor Blue -NoNewline
+Write-Host Done.
 
-Get-AppxPackage *Microsoft.BingNews* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.DesktopAppInstaller* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Office.OneNote* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Office.Sway* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.OneConnect* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.People* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.SkypeApp* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsAlarms* | Remove-AppxPackage
-Get-AppxPackage *microsoft.windowscommunicationsapps* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.WindowsFeedbackHub* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.Xbox.TCUI* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.XboxApp* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage
-Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage
-Get-AppxPackage *AdobeSystemsIncorporated.AdobePhotoshopExpress* | Remove-AppxPackage
-Get-AppxPackage *Duolingo-LearnLanguagesforFree* | Remove-AppxPackage
-Get-AppxPackage *PandoraMediaInc* | Remove-AppxPackage
-Get-AppxPackage *CandyCrush* | Remove-AppxPackage
-Get-AppxPackage *Wunderlist* | Remove-AppxPackage
-Get-AppxPackage *Flipboard* | Remove-AppxPackage
-Get-AppxPackage *Twitter* | Remove-AppxPackage
-Get-AppxPackage *Facebook* | Remove-AppxPackage
+
+Write-Host "[Benni_clean-win10] " -ForegroundColor Blue -NoNewline
+Write-Host Removing Windows 10 bloatware...
+foreach ($app in $ToRemove) {
+    Get-AppxPackage $app | Remove-AppxPackage
+}
+Write-Host "[Benni_clean-win10] " -ForegroundColor Blue -NoNewline
+Write-Host Done.
